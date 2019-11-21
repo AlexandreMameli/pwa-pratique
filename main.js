929401bfc183ca910f4c8fa15c17ca2b2569b7ac
alex mameli <alex.mameli@hotmail.fr>
2019-11-21 16:42:27 +0100

    notifs push ok

--- main.js
+++ main.js
@@ -6,9 +6,9 @@ function loadTechnologies(technos) {
             response.json()
                 .then(technos => {
                     const allTechnos = technos.map(t => `<div><b>${t.name}</b> ${t.description}  <a href="${t.url}">site de ${t.name}</a> </div>`)
-                            .join('');
-            
-                    technosDiv.innerHTML = allTechnos; 
+                        .join('');
+
+                    technosDiv.innerHTML = allTechnos;
                 });
         })
         .catch(console.error);
@@ -16,44 +16,112 @@ function loadTechnologies(technos) {
 
 loadTechnologies(technos);
 
-	
-if(navigator.serviceWorker) {
-	
+
+if (navigator.serviceWorker) {
     navigator.serviceWorker
-	
         .register('sw.js')
-	
+        // 8.4 Récupération ou création d'une souscription auprès d'un push service
+
+        .then(registration => {
+            // tentative d'obtention d'une souscription
+
+            // public vapid key générée par web-push, en prod appel d'api via fetch plutôt que static
+
+            const publicKey = "BCkFQ35OjpKovHruvoPLaiGe06qjqbVeweGGG7O9jiVUGn9TmxQXcnAe85zO9YXzBhG4iRyxknK9V0FoU3vScXQ";
+
+            registration.pushManager.getSubscription().then(subscription => {
+                // Déjà une souscription, on l'affiche
+                if (subscription) {
+                    console.log("subscription", subscription);
+                    extractKeysFromArrayBuffer(subscription);
+                    return subscription;
+                }
+                // Pas de souscription
+                else {
+                    // demande de souscription (clef convertie en buffer pour faire la demande)
+                    const convertedKey = urlBase64ToUint8Array(publicKey);
+                    return registration.pushManager.subscribe({
+                        userVisibleOnly: true,
+                        applicationServerKey: convertedKey
+                    })
+                        .then(newSubscription => {
+
+                            // Affiche le résultat pour vérifier
+
+                            console.log('newSubscription', newSubscription);
+
+                            extractKeysFromArrayBuffer(newSubscription);
+
+                            return newSubscription;
+
+                        })
+                }
+            })
+        })
         .catch(err => console.error('service worker NON enregistré', err));
-	
 }
 
 
 
+function extractKeysFromArrayBuffer(subscription) {
+    // no more keys proprety directly visible on the subscription objet. So you have to use getKey()
+    const keyArrayBuffer = subscription.getKey('p256dh');
+    const authArrayBuffer = subscription.getKey('auth');
+    const p256dh = btoa(String.fromCharCode.apply(null, new Uint8Array(keyArrayBuffer)));
+    const auth = btoa(String.fromCharCode.apply(null, new Uint8Array(authArrayBuffer)));
+    console.log('p256dh key', keyArrayBuffer, p256dh);
+    console.log('auth key', authArrayBuffer, auth);
+    // Paramètres nécessaires à l'objet de notification pushSubscription
+    console.log('endpoint :');
+    console.dir(subscription.endpoint);
+    console.log('p256dh key :', p256dh);
+    console.log('auth key :', auth);
+}
+
+
+
+// 8.4 Récupération ou création d'une souscription auprès d'un push service
+
+// Fonction pour convertir string en array buffer pour envoie au push service
+
+function urlBase64ToUint8Array(base64String) {
+    const padding = '='.repeat((4 - base64String.length % 4) % 4);
+    const base64 = (base64String + padding)
+        .replace(/-/g, '+')
+        .replace(/_/g, '/');
+    const rawData = window.atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
+    for (let i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+}
+
 // // 7.1 Notifications non persistantes
-	
+
 // // Vérifie si la fonctionalité est disponible et si 
-	
+
 // // l'utilisateur n'a pas refusé les notifications
-	
+
 // if(window.Notification && window.Notification !== "denied"){
-	
+
 //     // demande une permission
-	
+
 //     Notification.requestPermission(perm => {
-	
+
 //         // vérifie si la permission est acceptée par l'utilisateur
-	
+
 //         if(perm === "granted"){
-	
-            
-	
+
+
+
 //             // 7.2 Option de la notification
-	
+
 //             const options = {
-	
+
 //                 body : "Body de la notification",
 //                 icon : "images/icons/icon-72x72.png"
-	
+
 //             }
 //             // On crée une nouvelle notification
 //             // 7.2 On passe les options en deuxième argument
